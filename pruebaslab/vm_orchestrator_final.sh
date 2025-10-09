ca1c796d5ea2150373bd498d03938298abd5b896
pieroefe <94321767+pieroefe@users.noreply.github.com>
2025-10-09 01:53:22 -0500

    Update vm_orchestrator_final.sh

--- pruebaslab/vm_orchestrator_final.sh
+++ pruebaslab/vm_orchestrator_final.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+cd /home/ubuntu
+
+bash ./vm_orchestrator_fase1.sh
+bash ./vm_orchestrator_fase2.sh
+
+echo "----------------------------------------------------"
+echo "[OK] Topología completa desplegada."
+echo "----------------------------------------------------"
 
